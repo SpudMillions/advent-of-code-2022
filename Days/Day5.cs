377f105a5e6ff96eb4eceb3fb60d5515a8722750
Josh <joshuaadams18@gmail.com>
2022-12-05 14:48:56 -0500

    feat: Supply Stacks
    - get top packages from piles based on LIFO order
    - get top packages from piles based on FIFO order

--- Days/Day5.cs
+++ Days/Day5.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode.Days
 {
     public class Day5 : DayBase
     {
-        internal Day5() : base(5, 2022, "TBD")
+        internal Day5() : base(5, 2022, "Supply Stacks")
         {
         }
 
@@ -19,6 +19,8 @@ namespace AdventOfCode.Days
                     .Replace("]", " ")
                 into characters
                 select characters.ToList()).ToList();
+            
+            var packageRowsCopy = packageRows.Select(row => row.ToList()).ToList();
 
             foreach (var command in from line in Input
                 where line.StartsWith("move")
@@ -33,20 +35,27 @@ namespace AdventOfCode.Days
                 var levelToStore = numberOfMoves;
                 var from = int.Parse(command[1]) * 4 - 3;
                 var to = int.Parse(command[2]) * 4 - 3;
+                var numberOfPackagesToLift = 0;
 
                 while (numberOfMoves > 0)
                 {
                     var newPackageRows = new List<char>();
+                    var newPackageRowsCopy = new List<char>();
                     var fromIndex = packageRows.FindIndex(row => row[from] != 32);
-
+                    var fromIndexCopy = packageRowsCopy.FindIndex(row => row[from] != 32);
                     for (var i = 0; i < 35; i++)
                     {
                         newPackageRows.Add((char)32);
+                        newPackageRowsCopy.Add((char)32);
                     }
 
                     newPackageRows.Insert(to, packageRows[fromIndex][from]);
+                    newPackageRowsCopy.Insert(to, packageRowsCopy[fromIndexCopy][from]);
                     packageRows[fromIndex][from] = (char)32;
+                    packageRowsCopy[fromIndex][from] = (char)32;
                     packageRows.Insert(0, newPackageRows); 
+                    packageRowsCopy.Insert(0 + numberOfPackagesToLift, newPackageRowsCopy);
+                    numberOfPackagesToLift += 1;
                     numberOfMoves -= 1;
                 }
             }
@@ -63,8 +72,21 @@ namespace AdventOfCode.Days
                 packageLineIndex += 4;
             }
             
-            Console.WriteLine(topRowPackages);
+            var packageRowsCopyString = "";
+            var packageRowsCopyIndex = 1;
+            while (packageRowsCopyIndex < 34)
+            {
+                var packageIndex = packageRowsCopy.FindIndex(row => row[packageRowsCopyIndex] != 32);
+                if (packageIndex != -1)
+                {
+                    packageRowsCopyString += packageRowsCopy[packageIndex][packageRowsCopyIndex];
+                }
+
+                packageRowsCopyIndex += 4;
+            }
             
+            Console.WriteLine($"{GetType().Name}: {Title} --- First Order Top Packages: {topRowPackages}. Second Order Top Packages: {packageRowsCopyString}");
+
         }
     }
 }
