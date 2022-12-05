2e71c5c8cd8002cd2102c9af0710e3c2ad94d7e2
Josh <joshuaadams18@gmail.com>
2022-12-05 15:11:40 -0500

    wip: refactor duplication

--- Days/Day5.cs
+++ Days/Day5.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
@@ -13,6 +13,7 @@ namespace AdventOfCode.Days
 
         public override void Play()
         {
+            const char emptyChar = (char)32;
             var packageRows = (from line in Input
                 where !line.StartsWith("move")
                 select line.Replace("[", " ")
@@ -33,32 +34,47 @@ namespace AdventOfCode.Days
             {
                 var numberOfMoves = int.Parse(command[0]);
                 var levelToStore = numberOfMoves;
-                var from = int.Parse(command[1]) * 4 - 3;
-                var to = int.Parse(command[2]) * 4 - 3;
+                var from = ConvertInputUsingOffset(command[1]);
+                var to = ConvertInputUsingOffset(command[2]);
                 var numberOfPackagesToLift = 0;
 
                 while (numberOfMoves > 0)
                 {
                     var newPackageRows = new List<char>();
                     var newPackageRowsCopy = new List<char>();
-                    var fromIndex = packageRows.FindIndex(row => row[from] != 32);
-                    var fromIndexCopy = packageRowsCopy.FindIndex(row => row[from] != 32);
+                    
+                    var fromIndex = packageRows.FindIndex(row => row[from] != emptyChar);
+                    var fromIndexCopy = packageRowsCopy.FindIndex(row => row[from] != emptyChar);
                     for (var i = 0; i < 35; i++)
                     {
-                        newPackageRows.Add((char)32);
-                        newPackageRowsCopy.Add((char)32);
+                        newPackageRows.Add(emptyChar);
+                        newPackageRowsCopy.Add(emptyChar);
                     }
 
                     newPackageRows.Insert(to, packageRows[fromIndex][from]);
                     newPackageRowsCopy.Insert(to, packageRowsCopy[fromIndexCopy][from]);
-                    packageRows[fromIndex][from] = (char)32;
-                    packageRowsCopy[fromIndex][from] = (char)32;
+                    packageRows[fromIndex][from] = emptyChar;
+                    packageRowsCopy[fromIndex][from] = emptyChar;
                     packageRows.Insert(0, newPackageRows); 
                     packageRowsCopy.Insert(0 + numberOfPackagesToLift, newPackageRowsCopy);
                     numberOfPackagesToLift += 1;
                     numberOfMoves -= 1;
                 }
             }
+            var topRowPackages = TopRowPackages(packageRows);
+            var packageRowsCopyPackages = TopRowPackages(packageRowsCopy);
+            
+            Console.WriteLine($"{GetType().Name}: {Title} --- First Order Top Packages: {topRowPackages}. Second Order Top Packages: {packageRowsCopyPackages}");
+
+        }
+
+        private static int ConvertInputUsingOffset(string command)
+        {
+            return int.Parse(command) * 4 - 3;
+        }
+
+        private static string TopRowPackages(List<List<char>> packageRows)
+        {
             var topRowPackages = "";
             var packageLineIndex = 1;
             while (packageLineIndex < 34)
@@ -71,22 +87,8 @@ namespace AdventOfCode.Days
 
                 packageLineIndex += 4;
             }
-            
-            var packageRowsCopyString = "";
-            var packageRowsCopyIndex = 1;
-            while (packageRowsCopyIndex < 34)
-            {
-                var packageIndex = packageRowsCopy.FindIndex(row => row[packageRowsCopyIndex] != 32);
-                if (packageIndex != -1)
-                {
-                    packageRowsCopyString += packageRowsCopy[packageIndex][packageRowsCopyIndex];
-                }
 
-                packageRowsCopyIndex += 4;
-            }
-            
-            Console.WriteLine($"{GetType().Name}: {Title} --- First Order Top Packages: {topRowPackages}. Second Order Top Packages: {packageRowsCopyString}");
-
+            return topRowPackages;
         }
     }
 }
