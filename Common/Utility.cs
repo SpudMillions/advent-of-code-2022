af25455f2f18c66e818dfd5b3b89e7f52e699dca
Josh <joshuaadams18@gmail.com>
2022-12-13 14:01:49 -0500

    feat: Hill Climbing Algorithm
    - find shortest path from start to end
    - find shortest path from any start

---
+++ Common/Utility.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Days
+{
+    public class Utility
+    {
+        private static int[,] CreateMatrix(List<string>? input)
+        {
+            if (input == null) return new int[,] { };
+            var forest = new int[input.Count, input[0].Length];
+            var lineNumber = 0;
+            
+            foreach (var line in input)
+            {
+                var characterNumber = 0;
+                foreach (var s in line)
+                {
+                    forest[lineNumber, characterNumber] = int.Parse(s.ToString());
+                    characterNumber++;
+                }
+
+                lineNumber++;
+            }
+
+            return forest;
+        }
+    }
+}
